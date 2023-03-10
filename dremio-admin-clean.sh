21d34659cbe2a9e3397e057f0dfa1548703b6f4c
Brandon Jackson <usbrandon@gmail.com>
2023-03-10 10:56:50 -0600

    Update to Dremio 24

--- dremio-admin-clean.sh
+++ dremio-admin-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:23.0 clean --delete-orphans
-docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:23.0 clean --delete-orphan-profiles
-docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:23.0 clean --reindex-data
-docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:23.0 clean --compact
+docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:24.0 clean --delete-orphans
+docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:24.0 clean --delete-orphan-profiles
+docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:24.0 clean --reindex-data
+docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:24.0 clean --compact
