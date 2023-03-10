5464048ec2d7b06ecc6e0f4639140470784beb2a
Brandon Jackson <usbrandon@gmail.com>
2023-03-10 10:57:13 -0600

    Update to Dremio 24

--- dremio_upgrade_db.sh
+++ dremio_upgrade_db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:23.0 upgrade 
+docker run --entrypoint /opt/dremio/bin/dremio-admin --mount source=dremio_docker_dremio,target=/opt/dremio/data dremio/dremio-oss:24.0 upgrade 
